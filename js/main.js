36cb4e5cbc786145c31ea18deeec97ce604c0e5a
odan <odan3240@gmail.com>
2016-02-09 18:09:00 +0900

    データ入力部分の完成

--- js/main.js
+++ js/main.js
@@ -40,4 +40,45 @@ $(function() {
         clearMoves();
     });
     
+    function getStats(id) {
+        var ret = {};
+        var statsNames = ["hp", "atk", "def", "spa", "spd", "spe", "accuracy", "evasion"];
+        var tag = 'div#' + id + ' input[type=number].';
+        for (var i = 0; i < statsNames.length; i++) {
+            ret[statsNames[i]] = $(tag + statsNames[i]).val();
+        }
+        
+        return ret;
+    }
+    
+    function makePokemon(side) {
+        var p = {};
+        var toId = translation.toId;
+        p['species'] = toId('pokemon', $('fieldset#' + side + 'Pokemon .name').val());
+        p['ability'] = toId('ability', $('fieldset#' + side + 'Pokemon .ability').val());
+        p['item']    = toId('item', $('fieldset#' + side + 'Pokemon .item').val());
+        p['level']   = $('fieldset#' + side + 'Pokemon .level').val();
+        p['nature']  = $('fieldset#' + side + 'Pokemon .nature').val();
+        p['ivs']     = getStats(side + 'IVTab');
+        p['evs']     = getStats(side + 'EVTab');
+        
+        p['moves'] = [];
+        $('fieldset#' + side + 'Pokemon div.moves input').each(function(i, text) {
+            p['moves'].push(toId('move', $(text).val()));
+        });
+        
+        return p;
+    }
+    
+    function validInput() {
+        return true;
+    }
+    
+    $('button#calculate').on('click', function () {
+        if(!validInput()) {
+            return;
+        }
+        var p1 = makePokemon("my");
+        var p2 = makePokemon("opp");
+    });
 });
